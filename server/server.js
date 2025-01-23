aee60bd3d0fd004a564a8978a99ef3cc08719c9e
Swastik <98447347+Mark9-6@users.noreply.github.com>
2025-01-24 00:31:07 +0530

    cors conf

--- server/server.js
+++ server/server.js
@@ -25,6 +25,15 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', 'https://imagify-f-rho.vercel.app');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      return res.status(200).json({});
+    }
+    next();
+  });
 
 await connectDB();
  
